d57b639ba8957ddb2b32477f2c93a5aa44da325f
Matthew <mattj20@gmail.com>
2022-05-17 17:20:07 -0700

    commit tree 40c92f6747a661953c513e084a40a2f7e52b7bf5

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+require 'pry'
+def prime?(num)
+    test1 = (1..100).to_a
+    numbers = []
+    test1.each do |number|
+        numbers << number if num%number == 0
+    end
+    numbers << num if !numbers.include?(num)
+    if numbers.length > 2 || numbers.length == 1 || num < 0
+        return false
+    else
+        return true
+    end
+end
+
